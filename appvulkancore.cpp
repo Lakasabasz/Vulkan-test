8afda343531f29a6553a508c4c07e6b55fab95a8
Łakasabasz <lakasabasz@gmail.com>
2021-08-19 15:20:16 +0200

    Checkpoint

--- appvulkancore.cpp
+++ appvulkancore.cpp
@@ -844,29 +844,17 @@ void AppVulkanCore::mainLoop()
 
 void AppVulkanCore::cleanup()
 {
+    cleanupSwapChain();
+
+    vkDestroyBuffer(device, vertexBuffer, nullptr);
+    vkFreeMemory(device, vertexBufferMemory, nullptr);
+
     for(size_t i = 0; i<MAX_FRAMES_IN_FLIGHT; i++){
         vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
         vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
         vkDestroyFence(device, inFlightFences[i], nullptr);
     }
     vkDestroyCommandPool(device, commandPool, nullptr);
-
-    for(auto framebuffer : swapChainFramebuffers){
-        vkDestroyFramebuffer(device, framebuffer, nullptr);
-    }
-
-    vkDestroyPipeline(device, graphicsPipeline, nullptr);
-    vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
-    vkDestroyRenderPass(device, renderPass, nullptr);
-
-    for(auto imageView : swapChainImageViews){
-        vkDestroyImageView(device, imageView, nullptr);
-    }
-
-    vkDestroySwapchainKHR(device, swapChain, nullptr);
-
-    vkDestroyBuffer(device, vertexBuffer, nullptr);
-    vkFreeMemory(device, vertexBufferMemory, nullptr);
 
     vkDestroyDevice(device, nullptr);
 
@@ -883,8 +871,8 @@ void AppVulkanCore::cleanup()
 
 void AppVulkanCore::cleanupSwapChain()
 {
-    for(size_t i = 0; i < swapChainFramebuffers.size(); i++){
-        vkDestroyFramebuffer(device, swapChainFramebuffers[i], nullptr);
+    for(auto framebuffer : swapChainFramebuffers){
+        vkDestroyFramebuffer(device, framebuffer, nullptr);
     }
 
     vkFreeCommandBuffers(device, commandPool, commandBuffers.size(), commandBuffers.data());
